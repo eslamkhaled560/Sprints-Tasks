0efe8656b32c2f05d559432a6fb6eab51c5fc9a1
Islam <eslamkhaled560@gmail.com>
2023-05-15 21:52:16 +0300

    Validation Task Changes Added

--- 7- Django/Django Projects/S_DJ_01_Portfolio_Project/myapp/views.py
+++ 7- Django/Django Projects/S_DJ_01_Portfolio_Project/myapp/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
-from .models import Student, Course
+from django.shortcuts import redirect, render
+from .models import Student, Course, Post
+from .forms import PostForm
 
 
 def student_list(request):
@@ -12,3 +11,25 @@ def student_list(request):
 def course_list(request):
     courses = Course.objects.all()
     return render(request, 'course_list.html', {'courses': courses})
+
+
+def home_list(request):
+    homes = Post.objects.all()
+    return render(request, 'home_list.html', {'homes': homes})
+
+
+def home(request):
+    if request.method == 'POST':
+        details = PostForm(request.POST)
+
+        if details.is_valid():
+            post = details.save(commit=False)
+            post.save()
+            return redirect('home_list')
+
+        else:
+            return render(request, "home.html", {'form': details})
+
+    else:
+        form = PostForm(None)
+        return render(request, 'home.html', {'form': form})
